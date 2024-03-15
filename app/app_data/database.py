d6d602dacf24b0d267c828d83a249959cf78f4c3
unknown <alanuldashev@gmail.com>
2024-03-15 16:46:02 +0100

    working docker files

--- app/app_data/database.py
+++ app/app_data/database.py
@@ -8,12 +8,16 @@ import os
 dotenv_path = Path('app/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-SQL_DB = os.getenv('POSTGRES_DB') 
+
+SQL_DB = os.getenv('POSTGRES_DB')
 SQL_USER = os.getenv('POSTGRES_USER')
 SQL_DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+# SQL_DB = POSTGRES_DB
+# SQL_USER = POSTGRES_USER
+# SQL_DB_PASSWORD = POSTGRES_PASSWORD
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{SQL_USER}:{SQL_DB_PASSWORD}@localhost:5432/{SQL_DB}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{SQL_USER}:{SQL_DB_PASSWORD}@db:5432/{SQL_DB}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
