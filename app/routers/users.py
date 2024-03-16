a23201e589aebbd498103f763303f6321ad4f53f
Alan Yuldashev <alanuldashev@gmail.com>
2024-03-16 12:45:11 +0100

    login router fix

--- app/routers/users.py
+++ app/routers/users.py
@@ -54,7 +54,7 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440))
+    access_token_expires = timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440)))
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
