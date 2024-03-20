ff395fd9cd5b0d550d8def8b2c4570b133aaee4e
Alan Yuldashev <alanuldashev@gmail.com>
2024-03-20 18:22:38 +0100

    endpoints tests and bug fix

--- app/utils/password_security.py
+++ app/utils/password_security.py
@@ -4,11 +4,15 @@ from ..app_data import crud
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 def get_password_hash(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def authenticate_user(db: Session, username: str, password: str):
     user = crud.get_user_by_username(db, username)
