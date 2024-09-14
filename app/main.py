2fedda83d4e4240da570cd871db683538a0a8854
unknown <alanuldashev@gmail.com>
2024-09-14 18:10:17 +0200

    upload image endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import sections, users
+from .routers import sections, users, aws_interaction
 from .app_data import models, database
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -25,3 +25,5 @@ app.add_middleware(
 
 app.include_router(sections.router)
 app.include_router(users.router)
+app.include_router(aws_interaction.router)
+
