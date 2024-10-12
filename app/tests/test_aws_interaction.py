f300ae08309cec9ec61934f1196092fe5f74da99
unknown <alanuldashev@gmail.com>
2024-10-12 21:31:23 +0200

    removed commented code

--- app/tests/test_aws_interaction.py
+++ app/tests/test_aws_interaction.py
@@ -7,24 +7,9 @@ from ..dependencies import get_s3_client
 from botocore.exceptions import ClientError
 
 
-# mock_client = MagicMock()
-
-# mock_client_error = MagicMock()
-
 from ..app_data import schemas
 from ..dependencies import get_current_user
 
-# def mock_s3_client():
-#     mock_client.put_object.return_value = "https://fake-s3-url.com/test_image.jpg"
-#     return mock_client
-
-# def mock_s3_client_put_object_error():
-#     mock_client_error.put_object.side_effect = ClientError(
-#         {"Error": {"Code": "500", "Message": "Mocked error"}}, "PutObject"
-#     )
-#     return mock_client_error
-
-# app.dependency_overrides[get_s3_client] = mock_s3_client
 client = TestClient(app)
 
 def test_current_user():
@@ -83,8 +68,6 @@ def test_upload_image_too_large():
 
 
 def test_upload_image_error(mocked_s3_client_put_object_error):
-    # app.dependency_overrides[get_s3_client] = mock_s3_client_put_object_error
-
     with open("./app/tests/test_data/8.jpg", "rb") as image:
         unique_filename = f"test_image_name"
         response = client.post(
@@ -108,8 +91,6 @@ def test_upload_image_invalid_extension():
     assert response.json()["detail"] == "Invalid file extension"
 
 def test_upload_image_duplicate_filename(mocked_s3_client):
-    # mock_client.reset_mock()
-    # app.dependency_overrides[get_s3_client] = mock_s3_client
 
     with open("./app/tests/test_data/8.jpg", "rb") as image:
         unique_filename = f"test_image_name"
@@ -150,9 +131,6 @@ def mock_crud_functions(monkeypatch):
     return mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts
 
 def test_upload_website_success(mock_crud_functions, mock_get_current_user, mocked_s3_client):
-    # app.dependency_overrides[get_current_user] = mock_get_current_user
-    # mock_client.reset_mock()
-
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
     mock_get_domain.return_value = True
     mock_get_subdomain.return_value = None
@@ -171,8 +149,6 @@ def test_upload_website_success(mock_crud_functions, mock_get_current_user, mock
     assert response.status_code == 200
     assert mocked_s3_client.put_object.call_count == 2
 
-    # del app.dependency_overrides[get_current_user]
-
 from dataclasses import dataclass
 
 @dataclass
@@ -183,7 +159,6 @@ class MockSubdomainWithUser:
     user_id: int
 
 def test_upload_website_success_to_existing_subdomain(mock_crud_functions, mock_get_current_user, mocked_s3_client):
-    # app.dependency_overrides[get_current_user] = mock_get_current_user
 
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
     mock_get_domain.return_value = True
@@ -203,10 +178,8 @@ def test_upload_website_success_to_existing_subdomain(mock_crud_functions, mock_
     mock_create_subdomain.assert_not_called()
     mock_update_user_amounts.call_count == 1
 
-    # del app.dependency_overrides[get_current_user]
 
 def test_upload_website_invalid_file(mock_crud_functions, mock_get_current_user, mocked_s3_client):
-    # app.dependency_overrides[get_current_user] = mock_get_current_user
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
 
     with open("./app/tests/test_data/8.jpg", "rb") as image_file:
@@ -223,10 +196,7 @@ def test_upload_website_invalid_file(mock_crud_functions, mock_get_current_user,
     mock_create_subdomain.assert_not_called()
     mock_update_user_amounts.assert_not_called()
 
-    # del app.dependency_overrides[get_current_user]
-
-def test_upload_website_domain_not_exist(mock_crud_functions, mock_get_current_user):
-    # app.dependency_overrides[get_current_user] = mock_get_current_user
+def test_upload_website_domain_not_exist(mock_crud_functions, mock_get_current_user, mocked_s3_client):
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
     mock_get_domain.return_value = None
 
@@ -243,10 +213,9 @@ def test_upload_website_domain_not_exist(mock_crud_functions, mock_get_current_u
     mock_get_subdomain.assert_not_called()
     mock_create_subdomain.assert_not_called()
     mock_update_user_amounts.assert_not_called()
+    mocked_s3_client.put_object.assert_not_called()
 
-    # del app.dependency_overrides[get_current_user]
-
-def test_upload_website_no_uploads_left(mock_crud_functions):
+def test_upload_website_no_uploads_left(mock_crud_functions, mocked_s3_client):
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
 
     def override_get_current_user():
@@ -266,10 +235,10 @@ def test_upload_website_no_uploads_left(mock_crud_functions):
     mock_get_subdomain.assert_not_called()
     mock_create_subdomain.assert_not_called()
     mock_update_user_amounts.assert_not_called()
-
+    mocked_s3_client.put_object.assert_not_called()
     del app.dependency_overrides[get_current_user]
 
-def test_upload_website_no_subdomains_left(mock_crud_functions):
+def test_upload_website_no_subdomains_left(mock_crud_functions, mocked_s3_client):
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
     mock_get_subdomain.return_value = None
     
@@ -285,20 +254,17 @@ def test_upload_website_no_subdomains_left(mock_crud_functions):
             files={"zip_file": ("test_website.zip", zip_file, "application/zip")}
         )
 
-    print(response.json())
     assert response.status_code == 403
     assert "You don't have any subdomains left" in response.json()["detail"]
     mock_get_domain.assert_called_once_with(mock_get_domain.call_args[0][0], "example.com")
     mock_get_subdomain.assert_called_once()
     mock_create_subdomain.assert_not_called()
     mock_update_user_amounts.assert_not_called()
-    # mock_update_user_amounts.assert_called_once_with(mock_update_user_amounts.call_args[0][0], override_get_current_user())
+    mocked_s3_client.put_object.assert_not_called()
 
     del app.dependency_overrides[get_current_user]
 
-def test_upload_website_subdomain_not_available(mock_crud_functions, mock_get_current_user):
-    # app.dependency_overrides[get_current_user] = mock_get_current_user
-    # mock_client.reset_mock()
+def test_upload_website_subdomain_not_available(mock_crud_functions, mock_get_current_user, mocked_s3_client):
     mock_get_domain, mock_get_subdomain, mock_create_subdomain, mock_update_user_amounts = mock_crud_functions
     mock_get_domain.return_value = True
     mock_get_subdomain.return_value = MockSubdomainWithUser(id=1, name="test", domain_name="example.com", user_id=2)
@@ -316,5 +282,4 @@ def test_upload_website_subdomain_not_available(mock_crud_functions, mock_get_cu
     mock_get_subdomain.assert_called_once_with(mock_get_subdomain.call_args[0][0], "test", "example.com")
     mock_create_subdomain.assert_not_called()
     mock_update_user_amounts.assert_not_called()
-
-    # del app.dependency_overrides[get_current_user]
+    mocked_s3_client.put_object.assert_not_called()
